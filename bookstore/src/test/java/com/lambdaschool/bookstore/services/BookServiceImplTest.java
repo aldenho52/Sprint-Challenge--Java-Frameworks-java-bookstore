53f6b0b5ce2306fc914cefa1da120a9c3595048a
aldenho52 <69052933+aldenho52@users.noreply.github.com>
2020-12-18 10:00:21 -0700

    unit testing for BookService complete

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,6 +2,10 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
+import com.lambdaschool.bookstore.models.Author;
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
+import com.lambdaschool.bookstore.models.Wrote;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +14,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
@@ -39,26 +45,48 @@ public class BookServiceImplTest
     @Test
     public void findAll()
     {
+
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
+        assertEquals("Flatterland",
+            bookService.findBookById(26)
+                .getTitle());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void notFindBookById()
     {
+        assertEquals("Flatterland",
+            bookService.findBookById(1).getTitle());
     }
 
     @Test
     public void delete()
     {
+        bookService.delete(26);
+        assertEquals(4, bookService.findAll().size());
     }
 
     @Test
     public void save()
     {
+        Section s1 = new Section("Fiction");
+        s1.setSectionid(21);
+        Author a6 = new Author("Ian", "Stewart");
+        a6.setAuthorid(20);
+
+        Book b1 = new Book("Flatterland", "9780738206752", 2001, s1);
+        b1.getWrotes()
+            .add(new Wrote(a6, new Book()));
+
+        Book book1 = bookService.save(b1);
+
+        assertEquals("9780738206752",
+            book1.getIsbn());
     }
 
     @Test
